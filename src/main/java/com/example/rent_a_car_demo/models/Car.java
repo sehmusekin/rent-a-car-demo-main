40a081f96a4a4daf795e93c5e770b45fa60cec53
sehmusekin <146784285+sehmusekin@users.noreply.github.com>
2023-11-19 18:09:38 +0300

    arrangement

---
+++ src/main/java/com/example/rent_a_car_demo/models/Car.java
@@ -0,0 +1,47 @@
+package com.example.rent_a_car_demo.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "cars")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "year", nullable = false)
+    private Integer year;
+
+    @Column(name = "color", nullable = false, length = 20)
+    private String color;
+
+    @Column(name = "rental_fee", nullable = false)
+    private Double rentalFee;
+
+    @Column(name = "licence_plate", nullable = false, length = 11, unique = true)
+    private String licencePlate;
+
+    @ManyToOne
+    @JoinColumn(name = "model_id", nullable = false)
+    private Model model;
+
+    @ManyToOne
+    @JoinColumn(name = "car_type_id", nullable = false)
+    private CarType carType;
+
+    @OneToMany (mappedBy = "car")
+    private List<Rental> rentals;
+
+    @OneToMany(mappedBy = "car")
+    private List<Review> reviews;
+}
---
+++ src/main/java/com/example/rent_a_car_demo/models/Rental.java
@@ -0,0 +1,48 @@
+package com.example.rent_a_car_demo.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "rentals")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Rental {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "rental_start_date", nullable = false)
+    private Date rentalStartDate;
+
+    @Column(name = "rental_end_date", nullable = false)
+    private Date rentalEndDate;
+
+    @Column(name = "total_cost", nullable = false)
+    private Double totalCost;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id", nullable = false)
+    private Employee employee;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "car_id", nullable = false)
+    private Car car;
+
+    @OneToMany(mappedBy = "rental")
+    private List<Payment> payments;
+
+}
